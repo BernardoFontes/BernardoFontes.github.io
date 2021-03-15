979b0c637b91feee51fde22790ffc3562a385acc
BernardoFontes <bfontes.1997@gmail.com>
2021-03-15 15:44:42 -0300

    adicioando

---
+++ portfolio.jsx
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>HTML</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+  </head>
+  <body>
+  <h1> Bernardo Miranda de Castro Fontes </h1>
+  <h2> Índice </h2>
+  <div class= "indice">
+  <a href = "#Descrição" >Descrição</a> <br>
+  <a href = "#Lista" >Lista</a> <br>
+  <a href = "#Blog" >Blog</a> <br>
+</div>
+  <div id = "div"> <p id= "Descrição">
+  <img id = "foto" src = "https://raw.githubusercontent.com/BernardoFontes/BernardoFontes.github.io/master/assets/foto.png" alt="Bernardo Fontes em novembro de 2020">
+ <span class = "desc"> Brasileiro, 23 anos, morador do Rio de Janeiro, estudante de desenvolvimento de software na Trybe </span>  </p> </div>
+<ol id= "Lista">
+<li class="SW"> <strong> Javascript </strong> </li>
+<li class="Cozinha"> HTML </li>
+<li class="Moto"> CSS </li>
+<li class="Dev"> Pythona </li>
+</ol>
+<a href = "#foto"> Foto </a> <br>
+<a  id="Blog" href = "https://blog.betrybe.com/" target = "_blank"> Blog da Trybe! </a>
+</body>
+</html>
---
+++ portfolio/src/portfolio.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Portfolio extends React.Component{
+    render(){
+        return (
+          <div>
+            <h1> Bernardo Miranda de Castro Fontes </h1>
+            <img id = "foto" src = "https://raw.githubusercontent.com/BernardoFontes/BernardoFontes.github.io/master/assets/foto.png" />
+        <p> Desenvolvedor React.js, com conhecimentos em HTML, CSS, Javascript, Redux, SQL, Node.js, Python, aluno da Trybe, morador de São Paulo, 23 anos, buscando oportunidades na área de tecnologia. </p>
+        <h2> Contato </h2>
+        <div>
+        <span>Celular e Whatsapp: 21 99569-7990</span>
+        </div>
+        <div>
+        <span>E-mail: bfontes.1997@gmail.com</span>
+        </div>
+        <div>
+        <a href="https://www.linkedin.com/in/bernardo-fontes-/">Linkedin</a>
+        </div>
+        <div>
+        <a href="https://www.instagram.com/sw_berna/">Instagram</a>
+        </div>
+        <div>
+        <a href="https://github.com/BernardoFontes">GitHub</a>
+        </div>
+
+          </div>
+)
+}
+}
+export default Portfolio;
